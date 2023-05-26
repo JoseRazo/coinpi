cdaf1623b20eb536c5e05d9f66f31f0ebad653b4
Jose Razo <joserrp.13@gmail.com>
2023-05-26 11:09:40 -0600

    se elimino codigo api header

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HeadersService } from 'src/app/services/headers/headers.service';
-import { Header } from 'src/app/services/headers/header.interfaces';
 
 @Component({
   selector: 'app-header',
@@ -10,22 +8,11 @@ import { Header } from 'src/app/services/headers/header.interfaces';
 
 export class HeaderComponent implements OnInit {
 
-  headers: Header[];
+  constructor() {
 
-  constructor(private headerService: HeadersService) {
-    this.headers = [];
   }
 
   ngOnInit() {
-    this.headerService.getHeaders().subscribe(
-      (data: Header[]) => {
-        console.log(data);
-        this.headers = data;
-      },
-      (error: any) =>{
-        console.log(error);
-      }
-    );
   }
 
 }
