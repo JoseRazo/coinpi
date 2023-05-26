7aa809f8c9d07afefdb02d1562acd53dc30a7aa3
Jose Razo <joserrp.13@gmail.com>
2023-05-26 14:19:01 -0600

    se agrego la descripcion del evento en el footer

--- src/app/views/footer/footer.component.ts
+++ src/app/views/footer/footer.component.ts
@@ -28,4 +28,12 @@ export class FooterComponent implements OnInit {
     );
   }
 
+  truncateText(text: string, length: number): string {
+    if (text.length <= length) {
+      return text;
+    } else {
+      return text.substring(0, length) + '...';
+    }
+  }
+
 }
