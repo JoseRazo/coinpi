f0ff2cd36eac35bec4741e63ada121da856c472c
Jose Razo <joserrp.13@gmail.com>
2023-05-30 14:28:35 -0600

    cambio de url API produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://api.utsalamanca.edu.mx/api'
-  apiUrl: 'http://127.0.0.1:8001/api'
+  apiUrl: 'https://api.utsalamanca.edu.mx/api'
+  // apiUrl: 'http://127.0.0.1:8001/api'
 };
 
 /*
