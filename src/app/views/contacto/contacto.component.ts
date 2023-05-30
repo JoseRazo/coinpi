b7e5e8119345b3e486321cb7f91ec458d4cfa5d6
Jose Razo <joserrp.13@gmail.com>
2023-05-30 13:13:05 -0600

    creacion formulario de contacto

--- src/app/views/contacto/contacto.component.ts
+++ src/app/views/contacto/contacto.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -12,40 +13,57 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class ContactoComponent {
   formularioContacto: FormGroup;
+  enviadoIntentado: boolean = false; // Propiedad para controlar si se ha intentado enviar el formulario
 
   constructor(private http: HttpClient) {
     this.formularioContacto = new FormGroup({
-      nombre: new FormControl(''),
-      email: new FormControl(''),
-      telefono: new FormControl(''),
-      asunto: new FormControl(''),
-      mensaje: new FormControl('')
+      nombre: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(145)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      telefono: new FormControl('', Validators.required),
+      asunto: new FormControl('', Validators.required),
+      mensaje: new FormControl('', Validators.required)
     });
   }
 
   enviarFormulario() {
-    if (this.formularioContacto) {
-      const url = 'http://127.0.0.1:8001/api/enviar-formulario/'; // Reemplaza con la URL correcta de tu API REST
-      const formData = new FormData();
-      formData.append('nombre', this.formularioContacto.get('nombre')?.value);
-      formData.append('email', this.formularioContacto.get('email')?.value);
-      formData.append('telefono', this.formularioContacto.get('telefono')?.value);
-      formData.append('asunto', this.formularioContacto.get('asunto')?.value);
-      formData.append('mensaje', this.formularioContacto.get('mensaje')?.value);
-      
-      this.http.post(url, formData)
+    this.enviadoIntentado = true; // Se establece como verdadero cuando se hace clic en el botón enviar
+
+    if (this.formularioContacto.invalid) {
+      return; // Si el formulario es inválido, se detiene el proceso de envío y se muestran los mensajes de error
+    }
+
+    const url = 'http://127.0.0.1:8001/api/enviar-formulario/'; // Reemplaza con la URL correcta de tu API REST
+    const formData = new FormData();
+    formData.append('nombre', this.formularioContacto.get('nombre')?.value);
+    formData.append('email', this.formularioContacto.get('email')?.value);
+    formData.append('telefono', this.formularioContacto.get('telefono')?.value);
+    formData.append('asunto', this.formularioContacto.get('asunto')?.value);
+    formData.append('mensaje', this.formularioContacto.get('mensaje')?.value);
+
+    this.http.post(url, formData)
       .subscribe(
         response => {
           console.log('Formulario enviado con éxito');
-          // Puedes agregar una lógica adicional aquí, como mostrar un mensaje de éxito al usuario
+          Swal.fire({
+            title: 'Éxito',
+            text: '¡El formulario se ha enviado con éxito!\nEspere nuestra respuesta.',
+            icon: 'success'
+          });
+          this.formularioContacto.reset();
+          this.enviadoIntentado = false;
         },
         error => {
           console.error('Error al enviar el formulario', error);
-          // Puedes manejar el error aquí, como mostrar un mensaje de error al usuario
+          Swal.fire('Error', 'Ha ocurrido un error al enviar el formulario.', 'error');
+          Swal.fire({
+            title: 'Error',
+            text: 'Ha ocurrido un error al enviar el formulario.\nComunícate directamente al teléfono (464) 64 73861 ext.: 128 y 120\no al correo coinpi@utsalamanca.edu.mx.',
+            icon: 'error'
+          });
         }
       );
-    }
   }
+
 }
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.utsalamanca.edu.mx/api'
-  // apiUrl: 'http://127.0.0.1:8001/api'
+  // apiUrl: 'https://api.utsalamanca.edu.mx/api'
+  apiUrl: 'http://127.0.0.1:8001/api'
 };
